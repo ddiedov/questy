b5acab72d761f1c6ef6083228fc0ca6d057ebbdb
ddiedov <ddyedov@gmail.com>
2026-07-21 12:55:17 +0300

    richtext toolbar visibility - initial

--- app/static/js/rich_text.js
+++ app/static/js/rich_text.js
@@ -1,3 +1,29 @@
+// Запрещаем вложения
 document.addEventListener("trix-file-accept", (event) => {
     event.preventDefault();
+});
+
+// Показываем toolbar только во время редактирования
+document.querySelectorAll(".rich-text-container").forEach(container => {
+
+    const editor = container.querySelector("trix-editor");
+    const toolbar = document.getElementById(editor.getAttribute("toolbar"));
+
+    if (!toolbar) return;
+
+    container.addEventListener("focusin", () => {
+        console.log("focusin");
+        toolbar.classList.add("visible");
+    });
+
+    container.addEventListener("focusout", () => {
+        console.log("focusout");
+
+        setTimeout(() => {
+            if (!container.contains(document.activeElement)) {
+                toolbar.classList.remove("visible");
+            }
+        }, 0);
+    });
+
 });
